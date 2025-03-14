f0f2868e8bb1f547c56a6922db63973f9b450a89
Stephen Badcock <badcockstephen73@gmail.com>
2025-03-14 12:31:47 -0230

    adding triangle class to scalable folder q4.  testing added to demo4

--- Q4_interfaces/Demo4.java
+++ Q4_interfaces/Demo4.java
@@ -2,14 +2,16 @@ package Q4_interfaces;
 
 public class Demo4 {
     public static void main(String[] args) {
-        // Create Ellipse and Circle objects
+        // Create Ellipse, Circle, and Triangle objects
         Scalable ellipse1 = new Ellipse("Ellipse1", 10.0, 5.0);
         Scalable ellipse2 = new Ellipse("Ellipse2", 20.0, 10.0);
         Scalable circle1 = new Circle("Circle1", 7.0);
         Scalable circle2 = new Circle("Circle2", 15.0);
+        Scalable triangle1 = new Triangle("Triangle1", 3.0, 4.0, 5.0);
+        Scalable triangle2 = new Triangle("Triangle2", 6.0, 8.0, 10.0);
 
         // Store them in a Scalable array
-        Scalable[] shapes = { ellipse1, ellipse2, circle1, circle2 };
+        Scalable[] shapes = { ellipse1, ellipse2, circle1, circle2, triangle1, triangle2 };
 
         // Print the initial state
         System.out.println("Shapes before scaling:");
