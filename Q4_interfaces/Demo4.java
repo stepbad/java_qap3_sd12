2b236bed1b15214be2b841c1da291fd46b6ecf44
Stephen Badcock <badcockstephen73@gmail.com>
2025-03-14 12:25:31 -0230

    adding scalable to elipse and testing in demo4

---
+++ Q4_interfaces/Demo4.java
@@ -0,0 +1,34 @@
+package Q4_interfaces;
+
+public class Demo4 {
+    public static void main(String[] args) {
+        // Create Ellipse objects (using the Scalable and Shape interface functionality)
+        Scalable ellipse1 = new Ellipse("Ellipse1", 10.0, 5.0);
+        Scalable ellipse2 = new Ellipse("Ellipse2", 20.0, 10.0);
+
+        // Store them in a Scalable array
+        Scalable[] shapes = { ellipse1, ellipse2 };
+
+        // Print the initial state
+        System.out.println("Shapes before scaling:");
+        for (Scalable shape : shapes) {
+            System.out.println(shape);
+        }
+
+        // Scale the shapes by a factor of 1.5
+        scaleAllShapes(shapes, 1.5);
+
+        // Print the state after scaling
+        System.out.println("\nShapes after scaling:");
+        for (Scalable shape : shapes) {
+            System.out.println(shape);
+        }
+    }
+
+    // Static method to scale all shapes
+    public static void scaleAllShapes(Scalable[] shapes, double factor) {
+        for (Scalable shape : shapes) {
+            shape.scale(factor);
+        }
+    }
+}
---
+++ Q4_interfaces/Ellipse.java
@@ -0,0 +1,33 @@
+package Q4_interfaces;
+
+public class Ellipse extends Shape {
+    private double a; // Major axis
+    private double b; // Minor axis
+
+    public Ellipse(String name, double a, double b) {
+        super(name);
+        if (a >= b) {
+            this.a = a;
+            this.b = b;
+        } else {
+            this.a = b;
+            this.b = a;
+        }
+    }
+
+    @Override
+    public double computePerimeter() {
+        return Math.PI * (3 * (a + b) - Math.sqrt((3 * a + b) * (a + 3 * b)));
+    }
+
+    @Override
+    public double computeArea() {
+        return Math.PI * a * b;
+    }
+
+    @Override
+    public void scale(double factor) {
+        a *= factor;
+        b *= factor;
+    }
+}
